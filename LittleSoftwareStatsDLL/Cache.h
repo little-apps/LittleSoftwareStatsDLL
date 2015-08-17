2bb1f3777687226b31f0f78abce8f9ec634db971
Nick H <nick@little-apps.com>
2015-08-17 00:46:52 -0600

    renamed variables to proper names always uses unicode or multibyte (whichever is compiled) added try-catch blocks to CFile calls

--- LittleSoftwareStatsDLL/Cache.h
+++ LittleSoftwareStatsDLL/Cache.h
@@ -18,7 +18,7 @@
 
 #pragma once
 
-extern int nApiFormat;
+extern int g_nApiFormat;
 
 class Cache {
 public:
@@ -26,50 +26,50 @@ public:
 		LPTSTR szTempPath = new TCHAR[MAX_PATH];
 		CString strFile;
 
-		strApplicationId = strAppId;
+		m_strApplicationId = strAppId;
 
 		if (!GetTempPath(MAX_PATH, szTempPath)) {
 			delete[] szTempPath;
 			return;
 		}
 
-		if (nApiFormat == FORMAT_JSON)
-			strFile = StringFormat(_T("%s\\%s.json.tmp"), szTempPath, strApplicationId);
+		if (g_nApiFormat == FORMAT_JSON)
+			strFile = StringFormat(_T("%s\\%s.json.tmp"), szTempPath, m_strApplicationId);
 		else
-			strFile = StringFormat(_T("%s\\%s.xml.tmp"), szTempPath, strApplicationId);
+			strFile = StringFormat(_T("%s\\%s.xml.tmp"), szTempPath, m_strApplicationId);
 
 		delete[] szTempPath;
 
-		cFile.Open(strFile, CFile::modeCreate|CFile::modeNoTruncate|CFile::modeReadWrite);
+		m_cFile.Open(strFile, CFile::modeCreate | CFile::modeNoTruncate | CFile::modeReadWrite);
 
 		return;
 	}
 
 	~Cache() {
-		if (cFile != INVALID_HANDLE_VALUE)
-			cFile.Close();
+		if (m_cFile != INVALID_HANDLE_VALUE)
+			m_cFile.Close();
 	}
 
 	BOOL DataExists() {
-		if (cFile == INVALID_HANDLE_VALUE)
+		if (m_cFile == INVALID_HANDLE_VALUE)
 			return FALSE;
 
-		return (cFile.GetLength() > 0);
+		return (m_cFile.GetLength() > 0);
 	}
 
 	CStringW GetData() {
 		CStringW strData;
 
-		if (cFile == INVALID_HANDLE_VALUE)
+		if (m_cFile == INVALID_HANDLE_VALUE)
 			return L"";
 
 		// Go to beginning of file
-		cFile.SeekToBegin();
+		m_cFile.SeekToBegin();
 
 		// Read file
-		UINT nBytes = (UINT)cFile.GetLength() + 1;
+		UINT nBytes = (UINT)m_cFile.GetLength() + 1;
 		int nChars = nBytes / sizeof(WCHAR);
-		nBytes = cFile.Read(strData.GetBuffer(nChars), nBytes);
+		nBytes = m_cFile.Read(strData.GetBuffer(nChars), nBytes);
 		strData.ReleaseBuffer(nChars);
 
 		// Decode from Base64
@@ -82,7 +82,7 @@ public:
 		CStringW strOutput;
 
 		if (!DataExists()) {
-			if (nApiFormat == FORMAT_JSON) {
+			if (g_nApiFormat == FORMAT_JSON) {
 				strOutput = L"[";
 				strOutput += strData;
 				strOutput += L"]";
@@ -94,7 +94,7 @@ public:
 		} else {
 			CStringW strCachedData = GetData();
 
-			if (nApiFormat == FORMAT_JSON) {
+			if (g_nApiFormat == FORMAT_JSON) {
 				strOutput = L"[";
 				strOutput += strData;
 			} else {
@@ -105,7 +105,7 @@ public:
 			int nPos = 0;
 			CStringW strLine = strCachedData.Tokenize(L"\n", nPos).Trim();
 			while (strLine != L"") {
-				if (nApiFormat == FORMAT_JSON) {
+				if (g_nApiFormat == FORMAT_JSON) {
 					strOutput += L",";
 					strOutput += strLine;
 				} else {
@@ -115,7 +115,7 @@ public:
 				strLine = strCachedData.Tokenize(L"\n", nPos).Trim();
 			}
 
-			if (nApiFormat == FORMAT_JSON) {
+			if (g_nApiFormat == FORMAT_JSON) {
 				strOutput += L"]";
 			} else {
 				strOutput += L"</data>";
@@ -127,7 +127,7 @@ public:
 	}
 
 	BOOL SaveData(CStringW strData) {
-		if (cFile == INVALID_HANDLE_VALUE)
+		if (m_cFile == INVALID_HANDLE_VALUE)
 			return FALSE;
 
 		CStringW strOutput = strData + L"\n";
@@ -137,34 +137,38 @@ public:
 			strOutput += strOldData;
 		}
 
-		CStringW strBase64Output = base64_encode(strOutput);
+		CString strBase64Output = base64_encode(strOutput);
 
 		// Clear File
 		Clear();
 
-		// Write file (double string length since were using unsigned char)
-		cFile.Write(strBase64Output, strBase64Output.GetLength()*2);
+		TRY {
+			// Write file (double string length since were using unsigned char)
+			m_cFile.Write(strBase64Output, strBase64Output.GetLength() * 2);
 
-		// Set hidden permission
-#ifdef UNICODE
-		LPCWSTR szFileName = cFile.GetFilePath();
-		::SetFileAttributesW(szFileName, FILE_ATTRIBUTE_HIDDEN);
-#else
-		LPCSTR szFileName = cFile.GetFilePath();
-		::SetFileAttributesA(szFileName, FILE_ATTRIBUTE_HIDDEN);
-#endif
+			// Set hidden permission
+			::SetFileAttributes(m_cFile.GetFilePath(), FILE_ATTRIBUTE_HIDDEN);
+		} CATCH (CFileException, e) {
+
+		}
+		END_CATCH
 
 		return TRUE;
 	}
 
 	void Clear() {
 		if (DataExists()) {
-			cFile.SetLength(0);
-			cFile.Flush();
+			TRY {
+				m_cFile.SetLength(0);
+				m_cFile.Flush();
+			} CATCH(CFileException, e) {
+
+			}
+			END_CATCH
 		}
 	}
 private:
-	CFile cFile;
-	BOOL bFileExists;
-	CString strApplicationId;
+	CFile m_cFile;
+	BOOL m_bFileExists;
+	CString m_strApplicationId;
 };
